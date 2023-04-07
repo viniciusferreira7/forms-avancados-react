647faf67345ad19a185a1d39f8a7206631eec308
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-07 16:13:14 -0300

    feat: validating the data with zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import { useForm } from 'react-hook-form'
 import './styles/global.css'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const createUserFormSchema = z.object({
+  email: z
+    .string()
+    .nonempty('O e-mail é obrigatório')
+    .email('Formato de e-mail inválido'),
+  password: z.string().min(6, 'A senha precisa de no mínimo 6 caracteres'),
+})
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>
 
 export function App() {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  })
 
   function createUser(data: any) {
     console.log(data)
@@ -20,8 +38,9 @@ export function App() {
           <input
             type="text"
             className="border border-zinc-200 shadow-sm rounded h-10 bg-zinc-800 text-white px-3"
-            {...register('name')}
+            {...register('email')}
           />
+          {errors.email && <span>{errors.email.message}</span>}
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="">Senha</label>
@@ -30,6 +49,7 @@ export function App() {
             className="border border-zinc-200 shadow-sm rounded h-10 bg-zinc-800 text-white px-3"
             {...register('password')}
           />
+          {errors.password && <span>{errors.password.message}</span>}
         </div>
         <button
           type="submit"
