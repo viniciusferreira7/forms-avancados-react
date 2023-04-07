07db9bb5ed32fb752ff000e1f437f7da637673b0
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-07 14:38:45 -0300

    style: create form in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
 import './styles/global.css'
 
 export function App() {
-  return <h1 className="h-screen bg-zinc-950">Hello world</h1>
+  return (
+    <main className="h-screen bg-zinc-50 flex items-center justify-center">
+      <form className="flex flex-col gap-4 w-full max-w-xs">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">E-mail</label>
+          <input
+            type="text"
+            name="email"
+            className="border border-zinc-200 shadow-sm rounded h-10"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">Senha</label>
+          <input
+            type="password"
+            name="password"
+            className="border border-zinc-200 shadow-sm rounded h-10 "
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-emerald-500 rounded font-semibold h-10 text-white hover:bg-emerald-600"
+        >
+          Salvar
+        </button>
+      </form>
+    </main>
+  )
 }
