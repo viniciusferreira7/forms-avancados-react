dbcff00934a13eb5a15968f1a7d199d37bd618b3
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-07 15:08:56 -0300

    feat: integrated with react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
+import { useForm } from 'react-hook-form'
 import './styles/global.css'
 
 export function App() {
+  const { register, handleSubmit } = useForm()
+
+  function createUser(data: any) {
+    console.log(data)
+  }
+
   return (
-    <main className="h-screen bg-zinc-50 flex items-center justify-center">
-      <form className="flex flex-col gap-4 w-full max-w-xs">
+    <main className="h-screen bg-zinc-950 text-white flex items-center justify-center">
+      <form
+        onSubmit={handleSubmit(createUser)}
+        className="flex flex-col gap-4 w-full max-w-xs"
+      >
+        <h2 className="text-center text-3xl font-bold tracking-widest">FORM</h2>
         <div className="flex flex-col gap-1">
           <label htmlFor="">E-mail</label>
           <input
             type="text"
-            name="email"
-            className="border border-zinc-200 shadow-sm rounded h-10"
+            className="border border-zinc-200 shadow-sm rounded h-10 bg-zinc-800 text-white px-3"
+            {...register('name')}
           />
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="">Senha</label>
           <input
             type="password"
-            name="password"
-            className="border border-zinc-200 shadow-sm rounded h-10 "
+            className="border border-zinc-200 shadow-sm rounded h-10 bg-zinc-800 text-white px-3"
+            {...register('password')}
           />
         </div>
         <button
